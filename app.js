46f6bdd434c12bd384d20dc81b144a624f042e28
Darios2021 <87774283+Darios2021@users.noreply.github.com>
2023-08-26 15:08:59 -0300

    1

---
+++ app.js
@@ -0,0 +1,36 @@
+//Clase 01// Educación IT
+Clase 01 - Introduccion a la Programación JS
+
+
+/*
+Programar = Ralizar una secuencia de pasos logicos para obtener un resultado
+1=> Entender que tenemos que lograr (la maquina tiene que poder hacer algo)
+2=> Entender cuales son las herramientas para obetener el resultado deseado
+		=>Senetencias (oraciones)
+		=>Bloques de codigo (parrafos)
+		=>Programa (texto)
+
+Lenguajes de Programación = Serie de reglas sintactico gramaticales que permiten la comunicacion 
+entre los humanos y las maquinas.
+
+JavaScript = Permite la comunicacion con el navegador web
+=>Es necesario tener un archivo HTML 
+=>Utilizar la etiqueta Script para que en el mismo archivo escribir el codigo JS y agregarle el atri
+buto source para poner la ruta al archivo js. La etiqueta tieen que estar en la linea inmediatamente
+superior al cierre del body
+=>1995 Brendan eich
+=>Estandar de lenguaje - conjunto de reglas sintactio gramatical que hay que utilizar para escribir
+un lenguaje
+=> EMAScript 5 Estandar anterior
+=> EMAScript 6 (2015) no todos los navegadores entienden en su totalidad este estandar.
+*/
+
+//Sentencia
+document.write("Hola"); //sentendia irden que el navegar recibe
+
+
+/*
+Variable - Espacio de memoria que almacena datos que pueden cambiar a lo largo
+de la ejecucion del programa (la maquina realiza acciones que le ordenamos)
+=>Caja que tiene una etiqueta para identificarla
+*/
